6dcbb3b04ebac3ffdf44e9bc36ffce384e92ecb2
2468785842 <2468785842@qq.com>
2024-09-07 21:52:57 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Python灰帽子书籍实践项目
+
+虽然书比较老已经20多年了..., 但是里面的技术没有过时, 可以学习逆向技巧,
+需要注意一些Api已经变更,比如Python2是默认ASCII码, Python3变成Unicode了,
+
+## Win32 Api
+### ThreadContext
+
+* 线程上下文结构体:
+  * [WOW64CONTEXT](https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-wow64_context): x86
+  * [CONTEXT](https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-context): x64
+
+* 获取线程上下文:
+  * [Wow64GetThreadContext](https://github.com/MicrosoftDocs/sdk-api/blob/docs/sdk-api-src/content/winbase/nf-winbase-wow64getthreadcontext.md): x86
+  * [GetThreadContext](https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getthreadcontext): x64
+
+* 设置线程上下文
+  * [Wow64SetThreadContext](https://learn.microsoft.com/en-us/windows/win32/api/wow64apiset/nf-wow64apiset-wow64setthreadcontext): x86
+  * [SetThreadContext](https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-setthreadcontext): x64
---
+++ code/my_debugger.py
@@ -0,0 +1,126 @@
+from ctypes import *
+from my_debugger_defines import *
+
+kernel32 = windll.kernel32
+
+class debugger():
+    def __init__(self):
+        self.h_process       = None
+        self.pid             = None
+        self.debugger_active = False
+
+    def load(self, path_to_exe):
+        creation_flags = DEBUG_PROCESS
+
+        startupinfo         = STARTUPINFO()
+        process_information = PROCESS_INFORMATION()
+
+        startupinfo.dwFlags     = 0x1
+        startupinfo.wShowWindow = 0x0
+
+        startupinfo.cb = sizeof(startupinfo)
+
+        if kernel32.CreateProcessA(path_to_exe,
+                                   None,
+                                   None,
+                                   None,
+                                   creation_flags,
+                                   True,
+                                   None,
+                                   None,
+                                   byref(startupinfo),
+                                   byref(process_information)):
+
+            print("[*] We have sucessfully launched the process!")
+            print("[*] PID: %d" % process_information.dwProcessId)
+            self.h_process = self.open_process(process_information.dwProcessId)
+        else:
+            print("[*] Error: 0x%08x." % kernel32.GetLastError())
+
+    def open_process(self, pid):
+
+        h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, pid);
+        return h_process
+
+    def attach(self, pid):
+        self.h_process = self.open_process(pid)
+
+        if kernel32.DebugActiveProcess(pid):
+            self.debugger_active = True
+            self.pid             = int(pid)
+
+            # self.run()
+        else:
+            print("[*] Unable to attch to the process.")
+
+    def run(self):
+        while self.debugger_active == True:
+            self.get_debug_event()
+
+    def get_debug_event(self):
+        debug_event = DEBUG_EVENT()
+        continue_status = DBG_CONTINUE
+
+        if kernel32.WaitForDebugEvent(byref(debug_event), INFINITE):
+            # input("press a key to continue...")
+            # self.debugger_active = False
+            kernel32.ContinueDebugEvent(
+                debug_event.dwProcessId,
+                debug_event.dwThreadId,
+                continue_status
+            )
+
+    def detach(self):
+
+        if kernel32.DebugActiveProcessStop(self.pid):
+            print("[*] Finished debugging. Exiting...")
+            return True
+        else:
+            print("There was an error")
+            return False
+
+    def open_thread(self, thread_id):
+        h_thread = kernel32.OpenThread(
+            THREAD_ALL_ACCESS, None, thread_id
+        )
+
+        if h_thread != None:
+            return h_thread
+        else:
+            print("[*] Could not obtain a valid thread handle.")
+            return False
+
+    def enumerate_threads(self):
+        thread_entry = THREADENTRY32()
+        thread_list = []
+        snapshot = kernel32.CreateToolhelp32Snapshot(
+            TH32CS_SNAPTHREAD, self.pid)
+
+        if snapshot != None:
+
+            thread_entry.dwSize = sizeof(thread_entry)
+            success = kernel32.Thread32First(snapshot, byref(thread_entry))
+
+            while success:
+                if thread_entry.th32OwnerProcessID == self.pid:
+                    thread_list.append(thread_entry.th32ThreadID)
+
+                success = kernel32.Thread32Next(
+                    snapshot, byref(thread_entry)
+                )
+
+            kernel32.CloseHandle(snapshot)
+            return thread_list
+        else:
+            return False
+
+    def get_thread_context(self, thread_id=None, h_thread=None):
+        context = WOW64_CONTEXT()
+        context.ContextFlags = CONTEXT_FULL | CONTEXT_DEBUG_REGISTERS
+
+        h_thread = self.open_thread(thread_id)
+        if kernel32.Wow64GetThreadContext(h_thread, byref(context)):
+            kernel32.CloseHandle(h_thread)
+            return context
+        else:
+            return False
